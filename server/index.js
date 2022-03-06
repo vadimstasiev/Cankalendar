56cdd45c8925f8556c1e0626feaec0283f62bc58
vadimstasiev <vadim444@hotmail.com>
2022-03-06 18:59:48 +0000

    rebased again

--- server/index.js
+++ server/index.js
@@ -5,14 +5,16 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use("/user", userRouter);
 
 const CONNECTION_URL = 'mongodb://DockerDiamond:MongoDBhunter24365fd@10.10.10.26';
 const PORT = process.env.PORT|| 5000;
