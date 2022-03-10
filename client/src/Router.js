9279fcda96f891b01c0c568752e0383e91d15f8b
vadimstasiev <vadim444@hotmail.com>
2022-03-10 16:38:15 +0000

    cleanup

--- client/src/Router.js
+++ client/src/Router.js
@@ -4,8 +4,7 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 
 // import PostDetails from './components/PostDetails/PostDetails';
 // import Navbar from './components/Navbar/Navbar';
-// import Home from './components/Home/Home';
-import Auth from './components/Auth/legacy/Auth';
+import Home from './components/Home/Home';
 import SignUp from './components/Auth/SignUp';
 import SignIn from './components/Auth/SignIn';
 // import CreatorOrTag from './components/CreatorOrTag/CreatorOrTag';
@@ -17,15 +16,15 @@ const AppRouter = () => {
   return (
         <Router>
           <Routes>
-            {/* <Route path="/" exact element={() => <Redirect to="/posts" />} /> */}
-            {/* <Route path="/posts" exact element={<Home/>} /> */}
+            <Route path="/signup" exact element={<SignUp/>} />
+            <Route path="/signin" exact element={<SignIn/>} />
+            {/* <Route path="/" exact element={<Home/>} />
+            <Route path="/" exact element={() => <Navigate to="/posts" />} />
+            <Route path="/posts" exact element={<Home/>} /> */}
             {/* <Route path="/posts/search" exact element={<Home/>} /> */}
             {/* <Route path="/posts/:id" exact element={<PostDetails/>} /> */}
             {/* <Route path={['/creators/:name', '/tags/:name']} element={<CreatorOrTag/>} /> */}
-            <Route path="/auth" exact element={<Auth /> } />
             {/* <Route path="/auth" exact element={() => (!user ? <Auth /> : <Navigate to="/posts" />)} /> */}
-            <Route path="/signup" exact element={<SignUp/>} />
-            <Route path="/signin" exact element={<SignIn/>} />
             <Route path="*" element={<NotFound/>}/>
           </Routes>
         </Router>
