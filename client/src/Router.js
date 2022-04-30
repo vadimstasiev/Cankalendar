5fe43701a54660557cf94472d7d6764f4dc89d08
vadimstasiev <vadim444@hotmail.com>
2022-04-30 12:08:50 +0100

    fixed undetermined cause for nav ui glitch

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
+import {JoinProject, CreateNewProject} from './components/JoinProject';
 import Home from './components/Home/Home';
 import SignUp from './components/Auth/SignUp';
 import SignIn from './components/Auth/SignIn';
@@ -9,8 +10,6 @@ import SignOut from './components/Auth/SignOut';
 import Tickets from './components/Tickets/Tickets';
 import Kanban from './components/Kanban/Kanban';
 import PrivateRoute from './components/PrivateRoute';
-import CreateProject from './components/CreateProject';
-import JoinProject from './components/JoinProject';
 
 const AppRouter = () => {
   const user = JSON.parse(localStorage.getItem('profile'));
@@ -22,13 +21,7 @@ const AppRouter = () => {
             <Route path="/signin" exact element={<SignIn/>} />
             <Route path="/signout" exact element={<SignOut/>} />
             <Route path="/" exact element={<Home/>} />
-
-            <Route path='/CreateProject' element={<PrivateRoute from="/CreateProject"/>}>
-              <Route path="/CreateProject" exact element={<CreateProject/>} />
-            </Route>   
-            <Route path='/JoinProject' element={<PrivateRoute from="/JoinProject"/>}>
-              <Route path="/JoinProject" exact element={<JoinProject/>} />
-            </Route>         
+                   
             <Route path='/Tickets' element={<PrivateRoute from="/Tickets"/>}>
               <Route path="/Tickets" exact element={<Tickets/>} />
               <Route path="/Tickets/:id" exact element={<Tickets/>} />
@@ -37,6 +30,12 @@ const AppRouter = () => {
               <Route path="/Kanban" exact element={<Kanban/>} />
               <Route path="/Kanban/:id" exact element={<Kanban/>} />
             </Route>
+            <Route path='/CreateNewProject' element={<PrivateRoute from="/CreateNewProject"/>}>
+              <Route path="/CreateNewProject" exact element={<CreateNewProject/>} />
+            </Route>   
+            <Route path='/JoinProject' element={<PrivateRoute from="/JoinProject"/>}>
+              <Route path="/JoinProject" exact element={<JoinProject/>} />
+            </Route>  
             {/* <Route path="/" exact element={() => <Navigate to="/posts" />} /> */}
             {/* <Route path="/posts" exact element={<Home/>} /> */}
             {/* <Route path="/posts/search" exact element={<Home/>} /> */}
--- client/src/components/JoinProject.js
+++ client/src/components/JoinProject.js
@@ -1,11 +1,9 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import Background from './Background';
 import SvgBackground from "./SvgBackground";
 import {FixedToggle} from "./ThemeToggle";
-import { useNavigate, useLocation } from "react-router-dom";
-import { Modal, Button } from 'antd';   
-import { SyncOutlined, CloseOutlined, CheckOutlined, ExclamationCircleOutlined  } from '@ant-design/icons';
-import { joinproject } from '../actions/auth';
+import { useNavigate } from "react-router-dom";
+import { joinproject, createproject } from '../actions/auth';
 import { useDispatch } from 'react-redux';
 
 
@@ -26,12 +24,6 @@ const JoinProject = (props) => {
     }
 
 
-
-    useEffect(() => {
-    }, []);
-
-
-    
     return (
         <Background>
         <SvgBackground>
@@ -67,4 +59,54 @@ const JoinProject = (props) => {
     )
 }
 
-export default JoinProject
+
+const CreateNewProject = (props) => {
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    const navigate = useNavigate()
+    const dispatch = useDispatch();
+    const [name, setName] = useState("");
+    const [error, setError] = useState("");
+
+
+    const submit = async () => {
+        dispatch(createproject({userEmail: user?.result.email, projectName: name}, navigate)).catch((error) => {
+            setError(error.response?.data?.message)
+        })
+    }
+
+    return (
+        <Background>
+        <SvgBackground>
+            <FixedToggle/>
+            
+            <div className="flex my-auto items-center justify-center">
+                <div className="grid bg-white dark:bg-zinc-900 rounded-lg shadow-xl w-11/12 md:w-9/12 lg:w-2/5">
+                    <div className="grid grid-cols-1 mt-5 mx-7">
+                        <label className="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold pb-5">Please enter a project name</label>
+                        <input value={name} onChange={e => setName(e.target.value)} className="py-2 px-3 rounded-lg border-2 border-gray-300 dark:bg-zinc-900 dark:border-zinc-500 text-zinc-700 dark:text-white mt-1 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" type="text" placeholder="Please enter a descriptive title" />
+                    </div>
+
+                    {
+                        error?
+                        <>
+                            <div className="bg-red-100 dark:bg-red-900 border-l-4 border-red-500 text-red-700 dark:text-red-300 p-4 mx-7 mt-3" role="alert">
+                                <p className="font-bold">{error}</p>
+                            </div>
+
+                            
+                        </>
+                        :
+                        null
+                    }
+                    <div className='flex items-center justify-center  md:gap-8 gap-4 pt-5 pb-5'>
+                        <button className='w-auto bg-gray-500 hover:bg-gray-700 rounded-lg shadow-xl font-medium text-white px-4 py-2' onClick={() => navigate('/')} >Cancel</button>
+                        <button className='w-auto bg-gray-500 hover:bg-gray-700 rounded-lg shadow-xl font-medium text-white px-4 py-2' onClick={() => submit()}>Submit</button>
+                    </div>
+                </div>
+            </div>
+        </SvgBackground>
+        </Background>
+    )
+}
+
+export {JoinProject, CreateNewProject}
